9bd16891676e726a32ea27da53f7d726270f5cd9
Liliia Galkina <vvlili@yandex.ru>
2025-09-08 14:11:15 +0300

    адаптив

--- js/script.js
+++ js/script.js
@@ -6,3 +6,15 @@ for(let i = 0; i < arrowsDown.length; i++) {
         textBlock[i].classList.toggle("text-block__item-text_none");
     })
 }
+
+//menu
+
+const burger = document.querySelector("#burger");
+const menu = document.querySelector("#menu");
+
+burger.addEventListener("click", func);
+
+function func() {
+  burger.classList.toggle("menu__burger_active");
+  menu.classList.toggle("menu__list_active");
+}
